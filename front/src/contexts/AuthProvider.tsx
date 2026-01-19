47815c3fb66364102746b6e7bae8c25bdc01a859
kevin voli <gbamblekevin@gmail.com>
2026-01-19 19:32:19 +0000

    en ligne

--- front/src/contexts/AuthProvider.tsx
+++ front/src/contexts/AuthProvider.tsx
@@ -56,7 +56,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     
     try {
       // Appel à votre API de login
-      const response = await axios.post('http://localhost:3003/auth/login', {
+      const response = await axios.post('http://148.230.112.175:3002/auth/login', {
         email,
         password,
       });
