04522a6d5bb0b14f931c16823ebe920e5b1f8a15
kevin voli <gbamblekevin@gmail.com>
2026-04-07 16:29:50 +0000

    corection de l'alerte

--- message_whatsapp/src/system-alert/system-alert.module.ts
+++ message_whatsapp/src/system-alert/system-alert.module.ts
@@ -4,18 +4,9 @@ import { ConfigModule } from '@nestjs/config';
 import { SystemAlertService } from './system-alert.service';
 import { SystemAlertController } from './system-alert.controller';
 import { WhapiChannel } from 'src/channel/entities/channel.entity';
-import { ProviderChannel } from 'src/channel/entities/provider-channel.entity';
 import { WhatsappChat } from 'src/whatsapp_chat/entities/whatsapp_chat.entity';
-import { WhatsappPoste } from 'src/whatsapp_poste/entities/whatsapp_poste.entity';
 import { SystemAlertConfig } from './entities/system-alert-config.entity';
-import { OutboundRouterService } from 'src/communication_whapi/outbound-router.service';
 import { CommunicationWhapiService } from 'src/communication_whapi/communication_whapi.service';
-import { CommunicationMetaService } from 'src/communication_whapi/communication_meta.service';
-import { CommunicationMessengerService } from 'src/communication_whapi/communication_messenger.service';
-import { CommunicationInstagramService } from 'src/communication_whapi/communication_instagram.service';
-import { CommunicationTelegramService } from 'src/communication_whapi/communication_telegram.service';
-import { ChannelService } from 'src/channel/channel.service';
-import { MetaTokenService } from 'src/channel/meta-token.service';
 import { LoggingModule } from 'src/logging/logging.module';
 import { NotificationModule } from 'src/notification/notification.module';
 
@@ -24,18 +15,11 @@ import { NotificationModule } from 'src/notification/notification.module';
     ConfigModule,
     LoggingModule,
     NotificationModule,
-    TypeOrmModule.forFeature([WhapiChannel, ProviderChannel, WhatsappChat, WhatsappPoste, SystemAlertConfig]),
+    TypeOrmModule.forFeature([WhapiChannel, WhatsappChat, SystemAlertConfig]),
   ],
   providers: [
     SystemAlertService,
-    OutboundRouterService,
     CommunicationWhapiService,
-    CommunicationMetaService,
-    CommunicationMessengerService,
-    CommunicationInstagramService,
-    CommunicationTelegramService,
-    ChannelService,
-    MetaTokenService,
   ],
   controllers: [SystemAlertController],
   exports: [SystemAlertService],
