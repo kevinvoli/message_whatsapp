044af428a6c6f8e10f280e9871d03600ba039e7e
kevin voli <gbamblekevin@gmail.com>
2026-04-25 12:09:39 +0000

    deploiement corrige

--- message_whatsapp/src/gicop-report/report-submission.service.ts
+++ message_whatsapp/src/gicop-report/report-submission.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, Logger } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
