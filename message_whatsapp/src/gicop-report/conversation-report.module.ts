61b79e227f040434aa7c1228568ee423c2c8b6a2
kevin voli <gbamblekevin@gmail.com>
2026-04-25 08:45:20 +0000

    correction depandence

--- message_whatsapp/src/gicop-report/conversation-report.module.ts
+++ message_whatsapp/src/gicop-report/conversation-report.module.ts
@@ -7,13 +7,14 @@ import { ReportSubmissionService } from './report-submission.service';
 import { WhatsappCommercial } from 'src/whatsapp_commercial/entities/user.entity';
 import { Contact } from 'src/contact/entities/contact.entity';
 import { ContactPhone } from 'src/client-dossier/entities/contact-phone.entity';
+import { ClientDossier } from 'src/client-dossier/entities/client-dossier.entity';
 import { WhatsappChat } from 'src/whatsapp_chat/entities/whatsapp_chat.entity';
 import { OrderWriteModule } from 'src/order-write/order-write.module';
 import { ReportClosureMirrorListener } from './listeners/report-closure-mirror.listener';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([ConversationReport, WhatsappCommercial, Contact, ContactPhone, WhatsappChat]),
+    TypeOrmModule.forFeature([ConversationReport, WhatsappCommercial, Contact, ContactPhone, ClientDossier, WhatsappChat]),
     OrderWriteModule,
   ],
   controllers: [ConversationReportController],
