23817629f38e18ee2124e4fe2041904893a2b1d6
kevin voli <gbamblekevin@gmail.com>
2026-06-05 17:37:46 +0000

    instagramme logs

--- message_whatsapp/src/whapi/whapi.controller.ts
+++ message_whatsapp/src/whapi/whapi.controller.ts
@@ -432,6 +432,9 @@ export class WhapiController {
     @Query('hub.verify_token') token: string,
     @Query('hub.challenge') challenge: string,
   ) {
+    this.auditLogger.log(
+      `WEBHOOK_ACCEPTED request_id=${mode} provider=instagram tenant_id ig_account_id unknown`,
+    );
     if (mode === 'subscribe') {
       const matchesDb = await this.channelService.hasMatchingVerifyToken('instagram', token);
       if (matchesDb) {
@@ -453,6 +456,9 @@ export class WhapiController {
 
     this.assertPayloadSize(request.rawBody);
 
+    this.auditLogger.log(
+      `WEBHOOK_ACCEPTED request_id=${requestId} provider=instagram tenant_id ig_account_id unknown`,
+    );
     const igPayload = this.assertInstagramPayload(payload);
 
     const igAccountId = igPayload.entry?.[0]?.id;
